c9181ab55deb1345fda658207ffd170b52e6a3a6
David Marchante <cdmarchante@gmail.com>
2019-06-24 09:50:10 -0700

    first commit

---
+++ src/main/java/com/dmarchante/kiddoh/KidDohApplication.java
@@ -0,0 +1,13 @@
+package com.dmarchante.kiddoh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidDohApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidDohApplication.class, args);
+	}
+
+}
