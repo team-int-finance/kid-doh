aa474ec8afee455a25f844e96e183187a2d7214e
Charles Clemens <cclemensjr@gmail.com>
2019-06-24 14:02:46 -0700

    added id

--- src/main/java/com/dmarchante/kiddoh/models/Transaction.java
+++ src/main/java/com/dmarchante/kiddoh/models/Transaction.java
@@ -1,4 +1,29 @@
 package com.dmarchante.kiddoh.models;
 
+@Entity
 public class Transaction {
+    /****************
+     * Instance Variables
+     * */
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+
+/****************
+ * Constructors
+ * */
+
+
+/****************
+ * Getters / Setters
+ * */
+    public long getId() {
+        return id;
+    }
+
+
+/****************
+ * Instance Methods
+ * */
 }
