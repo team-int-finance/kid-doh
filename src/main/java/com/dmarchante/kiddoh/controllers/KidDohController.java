7af6c6ea72507045fd6af3b0364cd83e570efbe8
Robert Bronson <robert.j.bronson+cf@gmail.com>
2019-06-25 12:22:42 -0700

    login and sign up mostly done. Need user service impl data

--- src/main/java/com/dmarchante/kiddoh/controllers/KidDohController.java
+++ src/main/java/com/dmarchante/kiddoh/controllers/KidDohController.java
@@ -5,12 +5,28 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.security.Principal;
+
 @Controller
 public class KidDohController {
 
     @GetMapping("/")
-    public String getHomePage(@RequestParam (required = false, defaultValue = "")String error, Model m){
+    public String getHomePage(Model m, Principal p){
+        m.addAttribute("principal", p);
+        return p == null ? "Login" : "tempAccounts";
+//        return "Home";
+    }
+
+    @GetMapping("/signup")
+    public String getSignUpPage(@RequestParam (required = false, defaultValue = "")String error, Model m, Principal p){
+        m.addAttribute("principal", p);
         m.addAttribute("error", error);
-        return "Home";
+        return "SignUp";
+//        return "Home";
+    }
+
+    @GetMapping("/login")
+    public String logIn(){
+        return "Login";
     }
 }
