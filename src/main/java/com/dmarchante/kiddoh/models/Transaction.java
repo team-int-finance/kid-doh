51cce09a6c83eacaf435bebe591cd5fc279c0d2b
Charles Clemens <cclemensjr@gmail.com>
2019-06-25 10:21:45 -0700

    Updated category

--- src/main/java/com/dmarchante/kiddoh/models/Transaction.java
+++ src/main/java/com/dmarchante/kiddoh/models/Transaction.java
@@ -13,7 +13,7 @@ import java.util.Currency;
  * Enum Class
  * */
 enum Category {
-    Charity, Saving, Housing, Utilities, Food, Clothing, Transportation, Medical, Health, Insurance, Personal, Recreation, Debts;
+    Toys, VideoGames, Movies, Books, Food, Clothing
 }
 
 
