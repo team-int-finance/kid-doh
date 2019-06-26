32b724ac56e978195a1330dcfb08e970072c8f32
David Marchante <cdmarchante@gmail.com>
2019-06-26 15:21:39 -0700

    cleanpup unnecessary files

--- src/main/java/com/dmarchante/kiddoh/implementations/DashboardService.java
+++
@@ -1,8 +0,0 @@
-package com.dmarchante.kiddoh.implementations;
-
-import java.util.List;
-import java.util.Map;
-
-public interface DashboardService {
-    List<List<Map<Object, Object>>> getDashboardData();
-}
