601cf721a6b83689eafa7a6fb8673870cf780ef6
Robert Bronson <robert.j.bronson+cf@gmail.com>
2019-06-26 13:38:49 -0700

    bringing in changes from master

--- src/test/java/com/dmarchante/kiddoh/KidDohApplicationTests.java
+++ src/test/java/com/dmarchante/kiddoh/KidDohApplicationTests.java
@@ -1,16 +1,47 @@
 package com.dmarchante.kiddoh;
 
+import com.dmarchante.kiddoh.models.AppUser;
+import com.dmarchante.kiddoh.repositories.AppUserRepo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class KidDohApplicationTests {
+
+	@Autowired
+	MockMvc mockMvc;
+
+	@Autowired
+	AppUserRepo appUserRepo;
+
+
 
 	@Test
 	public void contextLoads() {
 	}
+
+	@Test
+	public void testHomeRoute() throws Exception {
+		mockMvc.perform(get("/")).andExpect(content().string(containsString("Kid-Doh")));
+	}
+
+	@Test
+	public void test_signUp() throws Exception {
+		mockMvc.perform(get("/signup")).andExpect(content().string(containsString("Page")));
+	}
+
+
 
 }
